b590b2e0494ec70ef2769f6c6871c624d591890b
Max Charles II <mcharles2016@yahoo.com>
2025-03-08 23:07:51 -0500

    changed function and added a non array/object version

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
+//Grabbing the bet min and max buttons from the DOM
 let betMin = document.querySelector('#betMin').addEventListener('click', spinSlotsMin);
 let betMax = document.querySelector('#betMax').addEventListener('click', spinSlotsMax);
 
+//Object that contains 6 options 
 const slotSymbols = {
     cherry: "🍒",
     lemon: "🍋",
@@ -10,7 +12,8 @@ const slotSymbols = {
     bell: "🔔"
 }
 //Object.values() is a built in function or method or prototype inside of javascript
-//Grabs all the values of the symbols and creates a new array of them
+//Grabs all the values of the slotSymbols array and creates a new array just the values
+//bankroll section grabbed from the DOM and currentBankroll variable created so the score can update based on the result of the spinSlotsMin or spinSlotsMax function
 const symbolsArray = Object.values(slotSymbols);
 let bankroll = document.querySelector('#bankroll');
 let currentBankroll = 1000;
@@ -29,6 +32,7 @@ function spinSlotsMin() {
     let slot3 = getRandomSymbol();
 
     //learn textContent, innerText, innerHTML in Anki
+    //textContent is faster than innerText and no styling is needed currently
     document.getElementById('slot1').textContent = slot1;
     document.getElementById('slot2').textContent = slot2;
     document.getElementById('slot3').textContent = slot3;
@@ -37,11 +41,11 @@ function spinSlotsMin() {
     if(slot1 === slot2 && slot2 === slot3) {
         currentBankroll += 50;
         bankroll.innerText = `Current Bankroll: ${currentBankroll}`;
-        document.getElementById('result').textContent = "you win";
+        document.getElementById('result').textContent = "cha-ching";
     } else {
         currentBankroll -=5;
-        bankroll.innerText = `Current Bank: ${currentBankroll}`;
-        document.getElementById('result').textContent = "you lose";
+        bankroll.innerText = `Current Bankroll: ${currentBankroll}`;
+        document.getElementById('result').textContent = "time to pay up";
     }
 }
 
@@ -52,6 +56,7 @@ function spinSlotsMax() {
     let slot3 = getRandomSymbol();
 
     //learn textContent, innerText, innerHTML in Anki
+    //textContent is faster than innerText and no styling needs to be added
     document.getElementById('slot1').textContent = slot1;
     document.getElementById('slot2').textContent = slot2;
     document.getElementById('slot3').textContent = slot3;
@@ -68,8 +73,11 @@ function spinSlotsMax() {
     }
 }
 
-//variable or inteeger to take money or give money ()
-
+//Things to consider for the future:
+//Player constructor function (player 1 vs player 2)
+//Set winning conditions (you win if over 2k /you lose if it hits 0)
+//Change you win to cha-ching and lose to time to pay up
+//Create an animation everytime you press the button before showing the final result
 //object person (name, last name, money)
 
 //add to the todo list and add or take things away
